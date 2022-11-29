cd20c2a50d667248f675fb4b52c1b5de7ede7621
Tate <tatebraeckel@gmail.com>
2022-11-28 18:53:58 -0700

    commit tree baf93e1a55f249354674dbe12928702298cd2927

---
+++ index.js
@@ -0,0 +1,156 @@
+function add(){
+
+}
+function subtract(){
+    
+}
+function multiply(){
+    
+}
+function divide(){
+    
+}
+
+function subtract(a,b) {
+    return a -b
+}
+
+function add(a,b) {
+    return a + b
+}
+
+function multiply(a,b) {
+    return a  * b
+}
+
+function divide(a,b) {
+    return a / b
+}
+
+function increment(n){
+    return n+ 1
+}
+
+function decrement(n){
+    return n - 1
+}
+
+function makeInt(n){
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseInt(n, 10)
+
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function add() {
+
+// }
+
+// function subtract() {
+
+// }
+// function multiply() {
+
+// }
+// function divide() {
+
+// }
+
+// function add(a, b) {
+
+//      return a + b;
+// }
+
+// console.log(add("23", "33"))
+
+// function subtract(a, b) {
+
+//     return a - b;
+// }
+
+// console.log(subtract("23", "33"))
+
+// function multiply(a, b) {
+
+//     return a * b;
+// }
+
+// console.log(multiply("23", "33"))
+
+// function divide(a, b) {
+
+//     return a / b;
+// }
+
+// console.log(divide("23", "33"))
+
+// number = 5;
+
+// function increment(n) {
+
+//     return ++n;
+// }
+
+// number = 5;
+
+// function decrement(n) {
+
+//     return --n;
+// }
+// function makeInt(n) {
+
+// const parsed = parseInt(`${n}, 10`);
+// return parsed
+// }
+
+// console.log(makeInt("3"))
+
+
+// function makeInt(n, m) {
+
+//     const parsed = parseInt(`${n}, ${m}`);
+//     return parsed;
+//     }
+    
+//     console.log(makeInt('3', '10'));
+
+//     function makeInt(n) {
+
+//         return parseInt(n, 10);
+//     }
+//     function preserveDecimal(n) {
+
+//         return parseFloat(n)
+//     }
